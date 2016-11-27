369f7e9c75d17b6008c8c9014638317e4575435f
Z-Ferguson <zfergusonn@gmail.com>
2016-11-27 18:11:26 -0500

    fixed database issues

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -1,4 +1,5 @@
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,8 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e$lx7q--7wo6d%*ccgul^10w-4qq)9k2^ltkp8kv%0u@hipp(('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -63,13 +63,6 @@ WSGI_APPLICATION = 'craigslist.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
