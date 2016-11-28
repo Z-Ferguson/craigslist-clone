f0230b5cf55544ae08dd28f37d0bb8398409cf7b
Z-Ferguson <zfergusonn@gmail.com>
2016-11-28 08:23:31 -0500

    fixed roots and urls

--- craigslist/settings.py
+++ craigslist/settings.py
@@ -102,4 +102,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
+USE_THOUSAND_SEPARATOR = True
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
